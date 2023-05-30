8e7ca12787f429677568811bdf59db01778a271c
Faisal Chakiki <faisalchakiki012018@gmail.com>
2023-05-30 11:23:33 +0700

    fix: auth form and create alert

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,16 @@
 <template lang="">
     <div class="w-full flex justify-center">
         <div class="container flex justify-center ">
-            <form @submit.prevent="handleLogin" action="" class="w-full border-[1px] p-4 sm:p-12 md:p-20 rounded-md my-12 max-w-2xl">
+            <div class="w-full border-[1px] p-4 sm:p-12 md:p-20 rounded-md my-12 max-w-2xl">
                 <div class="w-full flex flex-col items-center">
-                    <LogoFazz class="mb-5"/>
+                    <LogoFazz class="mb-3"/>
                     <p class="text-2xl font-semibold mb-2">Selamat Datang</p>
                     <p class="text-normal mb-3">Belum punya akun Fazzrack?
                         <span @click="moveLogin" class="text-[#ef6807] font-bold cursor-pointer">Daftar disini</span>
                     </p>
                 </div>
-                <div class="w-full">
+                <form @submit.prevent="handleLogin">
+                    <div class="w-full">
                     <div class="w-full mb-4">
                         <Input
                             type="email"
@@ -25,24 +26,25 @@
                             name='password'
                             placeholder='Masukan Kata Sandi'
                             @on-change="handleInput"/>
+                        </div>
                     </div>
-                </div>
-                <div class="w-full flex justify-between items-center">
-                    <div class="form-control">
-                        <label class="label cursor-pointer">
-                            <input type="checkbox" checked="checked" class="checkbox-xs mr-5 w-3"/>
-                            <span class="label-text">Ingat akun saya</span>
-                        </label>
+                    <div class="w-full flex justify-between items-center">
+                        <div class="form-control">
+                            <label class="label cursor-pointer">
+                                <input type="checkbox" checked="checked" class="checkbox-xs mr-5 w-3"/>
+                                <span class="label-text">Ingat akun saya</span>
+                            </label>
+                        </div>
+                        <div>
+                            <p class="text-[#2557a7] font-bold">Lupa Password?</p>
+                        </div>
                     </div>
-                    <div>
-                        <p class="text-[#2557a7] font-bold">Lupa Password?</p>
-                    </div>
-                </div>
-                <BtnPrimary size="btn-sm" title="masuk"/>
+                    <BtnPrimary size="btn-sm" title="masuk"/>
+                </form>
                 <p  class="w-full text-center my-4">atau masuk menggunakan</p>
                 <BtnGoogle title="google"/>
-            </form>
-            <ToastSuccess v-if="toastStatus" title="Succes Login"/>
+            </div>
+            <!-- <ToastSuccess v-if="toastStatus" title="Succes Login"/> -->
         </div>
     </div>
 </template>
@@ -52,12 +54,13 @@
     import BtnPrimary from "../components/atoms/BtnPrimary.vue"
     import BtnGoogle from '../components/atoms/BtnGoogle.vue';
     import LogoFazz from '../components/atoms/LogoFazz.vue';
-    import ToastSuccess from '../components/atoms/ToastSuccess.vue';
+    // import ToastSuccess from '../components/atoms/ToastSuccess.vue';
+    import Swal from 'sweetalert2';
     import axios from 'axios';
 
     interface Data {
         form: IForm
-        toastStatus:boolean
+        // toastStatus:boolean
     }
 
     interface IForm {
@@ -73,7 +76,7 @@
                     email: "",
                     password: ""
                 },
-                toastStatus:false
+                // toastStatus:false
             }
         },
         components: {
@@ -81,7 +84,7 @@
             BtnPrimary,
             BtnGoogle,
             LogoFazz,
-            ToastSuccess
+            // ToastSuccess
         },
         methods: {
             handleInput(data : any) {
@@ -90,19 +93,25 @@
                 } else {
                     this.form.password = data.value
                 }
-                // console.log(import.meta.env.MODE)
             },
             handleLogin(){
-                axios.post(`https://fazz-track-sample-api.vercel.app/login`, this.form)
-               .then((_res)=>{
-                localStorage.setItem('token', _res.data.data.token)
-                this.toastStatus = true
-                setTimeout(() => {
-                //    this.$router.push('/') 
-                    window.location.href = '/'
-                    this.toastStatus = false
-                }, 2000);
-               })
+                if(this.form.email === '' || this.form.password === ''){
+                    return Swal.fire('Please fill all the input', '', 'warning')
+                }else{
+                    axios.post(`https://fazz-track-sample-api.vercel.app/login`, this.form)
+                    .then((_res)=>{
+                        localStorage.setItem('token', _res.data.data.token)
+                        Swal.fire('Success Sign In', '', 'success')
+                        setTimeout(() => {
+                            window.location.href = '/'
+                        }, 2000);
+                    }).catch((_err) => {
+                        Swal.fire('Email or Password Wrong', '', 'error')
+                        setTimeout(() => {
+                            return window.location.reload()
+                        }, 2000);
+                    })
+                }
             },
             moveLogin(){
                 this.$router.push("/register")
