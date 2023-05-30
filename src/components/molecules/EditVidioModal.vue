b02e7c4a6b97be78ddbfdc135642bdfc90288e05
permana017 <ahdipermana07@gmail.com>
2023-05-30 12:48:24 +0700

    fix: model edit vidio

--- src/components/molecules/EditVidioModal.vue
+++ src/components/molecules/EditVidioModal.vue
@@ -75,8 +75,8 @@
                         <button
                             @click="handleEdit"
                             type="button"
-                            class="inline-flex w-full justify-center rounded-md bg-success px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-succes/70 sm:ml-3 sm:w-auto">Deactivate</button>
-                        <button
+                            class="inline-flex w-full justify-center rounded-md bg-success px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-succes/70 sm:ml-3 sm:w-auto">Save Edit</button>
+                            <button
                             @click="showModal"
                             type="button"
                             class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
